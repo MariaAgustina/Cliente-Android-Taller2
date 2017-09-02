df71c7b9e1d996b35f01878688faab85b3524913
AgustinaMarkosich <ma.markosich@gmail.com>
2017-09-02 16:40:08 -0300

    comento warning agregado en el OnCreate del main activity

--- app/src/main/java/com/taller2/llevame/MainActivity.java
+++ app/src/main/java/com/taller2/llevame/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         //Log
         Log.v(TAG,"Hello world");
-        Log.w(TAG,"Probando un warning");
+        //Log.w(TAG,"Probando un warning");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
