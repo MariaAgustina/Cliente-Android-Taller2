b74dfdd4dee1c05ac9157488087adb8995aa36b4
AgustinaMarkosich <ma.markosich@gmail.com>
2017-10-08 00:34:07 -0300

    cuando se muestra la loading view no se permite interaccion del usuario

--- app/src/main/java/com/taller2/llevame/ProfileActivity.java
+++ app/src/main/java/com/taller2/llevame/ProfileActivity.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.android.volley.VolleyError;
 import com.facebook.login.LoginManager;
 import com.taller2.llevame.Creational.FactoryActivities;
 import com.taller2.llevame.Models.Client;
 import com.taller2.llevame.Models.Session;
+import com.taller2.llevame.Views.LoadingView;
 import com.taller2.llevame.serviceLayerModel.ClientRequest;
 
 public class ProfileActivity extends BaseAtivity {
@@ -18,6 +20,7 @@ public class ProfileActivity extends BaseAtivity {
     private static final String TAG = "ProfileActivity";
     private Client client;
     private Session session;
+    private LoadingView loadingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,16 @@ public class ProfileActivity extends BaseAtivity {
         setContentView(R.layout.activity_main_menu);
         this.session = (Session) getIntent().getSerializableExtra("session");
 
+        this.setUpInitials();
+
         ClientRequest clientRequest = new ClientRequest();
         clientRequest.setClientEndPoint(this.session.client_id,this.session.type_client);
         clientRequest.getClient(this);
+    }
+
+    public void setUpInitials(){
+        this.loadingView = new LoadingView();
+        this.loadingView.setLoadingViewInvisible(this);
     }
 
     public void logout(View view){
@@ -50,7 +60,13 @@ public class ProfileActivity extends BaseAtivity {
 
     @Override
     public void onGetClientSuccess(Client client){
+        this.loadingView.setLoadingViewInvisible(this);
         this.client = client;
         findViewById(R.id.loadingPanel).setVisibility(View.GONE);
+    }
+
+    public void onServiceDidFailed(VolleyError error){
+        super.onServiceDidFailed(error);
+        this.loadingView.setLoadingViewInvisible(this);
     }
 }
--- app/src/main/java/com/taller2/llevame/Views/LoadingView.java
+++ app/src/main/java/com/taller2/llevame/Views/LoadingView.java
@@ -1,6 +1,7 @@
 package com.taller2.llevame.Views;
 
 import android.view.View;
+import android.view.WindowManager;
 
 import com.taller2.llevame.BaseAtivity;
 import com.taller2.llevame.R;
@@ -12,11 +13,18 @@ import com.taller2.llevame.R;
 public class LoadingView {
 
     public void setLoadingViewVisible(BaseAtivity activity){
+
         activity.findViewById(R.id.loadingPanel).setVisibility(View.VISIBLE);
+        activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
     }
 
     public void setLoadingViewInvisible(BaseAtivity activity){
+
         activity.findViewById(R.id.loadingPanel).setVisibility(View.INVISIBLE);
+        activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+
     }
 
 }
